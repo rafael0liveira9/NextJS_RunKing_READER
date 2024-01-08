22ad8311b698305fd1e26e53031a667e17c56f78
Rafael Oliveira <rafaelmo@pinolab.com.br>
2024-01-08 10:18:09 -0300

    att modal no conection

--- src/app/page.js
+++ src/app/page.js
@@ -5,15 +5,18 @@ import { useRouter } from "next/navigation";
 import Loading from "@/components/loading";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import ConfirmModal from "@/components/modal/confirmation";
 
 export default function Login() {
   const router = useRouter();
-  const [userName, setUsername] = useState()
-  const [userPassword, setPassword] = useState()
-  const [mask, setMask] = useState(true)
-  const [user, setUser] = useState({})
-  const [dataError, setDataError] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
+  const [userName, setUsername] = useState();
+  const [userPassword, setPassword] = useState();
+  const [mask, setMask] = useState(true);
+  const [user, setUser] = useState({});
+  const [dataError, setDataError] = useState(false);
+  const [conectionError, setConectionError] = useState(false);
+  const [modalConection, setModalConection] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const URL_API_RUNKING = "https://api.runking.com.br/"
 
@@ -38,6 +41,13 @@ export default function Login() {
     router.push("/home");
   }
 
+
+  // ******************
+
+  //   user: 'admin@admin.com.br',
+  //   password: 'runking2023!@',
+
+  // *******************
   async function signIn() {
     setIsLoading(true);
 
@@ -48,25 +58,36 @@ export default function Login() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          user: 'admin@admin.com.br',
-          password: 'runking2023!@',
+          user: userName,
+          password: userPassword
         }),
       });
 
-      if (!response.ok) {
-        throw new Error(`Erro na requisição: ${response.status} - ${response.statusText}`);
-      }
-
       const data = await response.json();
 
+      if (response.status >= 205) {
 
-      if (!data.length) {
+        console.log(response.status != 200)
+
+        if (response.status > 500) {
+          setModalConection(true)
+          setDataError(false)
+          setConectionError(true)
+        } else {
+          setConectionError(false)
+          setDataError(true)
+        }
+
+        throw new Error(`Erro na requisição: ${response.status} - ${response.statusText}`);
+      } else {
+
         localStorage.setItem("user_id", data.id);
         localStorage.setItem("user_name", data.name);
         localStorage.setItem("user_email", data.email);
         localStorage.setItem("user_phone", data.phone || "Não Preenchido");
         localStorage.setItem("user_jwt", data.jwt);
 
+        setConectionError(false)
         setDataError(false)
         setIsLoading(false);
         router.push("/select")
@@ -75,7 +96,6 @@ export default function Login() {
     } catch (error) {
       console.error("Erro na requisição:", error.message);
       setIsLoading(false);
-      setDataError(true)
     }
   }
 
@@ -100,6 +120,7 @@ export default function Login() {
   return (
     <main className="fullContainer">
       <Header title="Login"></Header>
+      {modalConection == true && <ConfirmModal confirm={() => router.push("/home")} cancel={() => setModalConection(false)} question={"Erro ao conectar-se a rede, deseja prosseguir sem internet"}></ConfirmModal>}
       <div className="mainContainer" style={{ width: "100%", maxWidth: "800px" }}>
         <div className="mainContainer gap-4" style={{ alignItems: "end" }}>
           <div className="w-full mb-5 flex justify-center">
@@ -132,7 +153,7 @@ export default function Login() {
             ></img>
           </div>
           {dataError === true && <p className="errorInput"> * O e-mail ou Senha digitados não são validos.</p>}
-
+          {conectionError === true && <p className="errorInput"> * Erro de conexão, conecte-se a uma rede para Logar.</p>}
           <button
             className='btnGreen'
             style={{ width: "100%" }}
